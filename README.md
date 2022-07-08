1b73d2c75d27a51e8ef86561d327020439dacc47
Ludwig Prager <ludwig.prager@celp.de>
2022-07-08 17:41:09 +0200

    disables my settings

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## TL;DR
 1. Set your project id in  
-[`MY_project_id`](./set-env.sh#L8)
+[`MY_project_id`](./set-env.sh#L7)
 in
 [`set-env.sh`](./set-env.sh)  
 and select a  
-[`MY_PREFIX`](./set-env.sh#L7)
+[`MY_PREFIX`](./set-env.sh#L15)
 in
 [`set-env.sh`](./20-tf-backend/set-env.sh).
 The latter must be globally unique.
--- set-env.sh
+++ set-env.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
+# add the project id that GCP generated for your project
+# example:
+# export MY_project_id="g1-01-355707"
+
+# export MY_project_id=
+
+export CLOUDSDK_CORE_PROJECT=${MY_project_id}
+
 # set MY_PREFIX so that BUCKET_NAME is a globally unique name
 # example:
-MY_PREFIX=g1-20220708-lp
+# MY_PREFIX=g1-20220708-lp
 
 # MY_PREFIX=
 
 export BUCKET_NAME="${MY_PREFIX}-bucket"
 
-# add the project id that GCP generated for your project
-# example:
-export MY_project_id="g1-01-355707"
 
-# export MY_project_id=
-
-export CLOUDSDK_CORE_PROJECT=${MY_project_id}
